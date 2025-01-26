05eb92bdc8c5f3c882465b434361f347909746a8
hunga516 <hunga5160@gmail.com>
2025-01-26 23:50:50 +0700

    change amount qr code

--- src/app/qr-code/page.tsx
+++ src/app/qr-code/page.tsx
@@ -1,7 +1,7 @@
 export default function QrCodePage() {
     return (
         <div className="pb-96 w-full mx-auto">
-            <img src="https://img.vietqr.io/image/970436-1028422873-qr_only.png?amount=500000&addInfo=GTA%VI%736&accountName=LENGOCLOC" alt="qrcode"/>
+            <img src="https://img.vietqr.io/image/970436-1028422873-qr_only.png?amount=100000&addInfo=GTA%VI%736&accountName=LENGOCLOC" alt="qrcode"/>
         </div>
     )
 }
