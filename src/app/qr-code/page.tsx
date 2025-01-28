2628b6116de6a058a5e0250395870ecca975a10f
hunga516 <hunga5160@gmail.com>
2025-01-28 22:55:56 +0700

    remove padding bottom

--- src/app/qr-code/page.tsx
+++ src/app/qr-code/page.tsx
@@ -1,6 +1,6 @@
 export default function QrCodePage() {
     return (
-        <div className="pb-96 w-full mx-auto">
+        <div className="w-full mx-auto">
             <img src="https://img.vietqr.io/image/970436-1028422873-qr_only.png?amount=100000&addInfo=GTA%VI%736&accountName=ACCOUNT1" alt="qrcode"/>
         </div>
     )
