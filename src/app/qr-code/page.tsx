19c4e0b192d91585ba786e39f13f5358d76270c0
hunga516 <hunga5160@gmail.com>
2025-02-03 23:17:46 +0700

    change account name

--- src/app/qr-code/page.tsx
+++ src/app/qr-code/page.tsx
@@ -1,7 +1,7 @@
 export default function QrCodePage() {
     return (
         <div className="w-full mx-auto">
-            <img src="https://img.vietqr.io/image/970436-1028422873-qr_only.png?amount=100000&addInfo=GTA%VI%736&accountName=ACCOUNT1" alt="qrcode"/>
+            <img src="https://img.vietqr.io/image/970436-1028422873-qr_only.png?amount=100000&addInfo=GTA%VI%736&accountName=ACCOUNT12" alt="qrcode"/>
         </div>
     )
 }
