4bb218db902c44cd170bf141fd6ff8c5745da172
hunga516 <hunga5160@gmail.com>
2025-02-09 00:21:50 +0700

    add dynamicroute handler with params.id always is string

---
+++ src/app/api/user/[id]/route.ts
@@ -0,0 +1,12 @@
+import {users} from "@/app/api/user/route";
+
+export async function GET(req: Request, { params } : {
+    params: {
+        id: string;
+    }
+}) {
+    const { id } = await params
+    const user = users.find(user => user.id === parseInt(id))
+
+    return Response.json(user);
+}
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,9 +1,11 @@
-const users = [
+export const users = [
     {
+        id: 1,
         name: 'Le Ngoc Loc',
         age: 21,
     },
     {
+        id: 2,
         name: 'Nguyen Bao Tran',
         age: 21,
     }
@@ -11,4 +13,14 @@ const users = [
 
 export async function GET() {
     return Response.json(users)
+}
+
+export async function POST(request: Request) {
+    const user = await request.json();
+    const newUser = { id: users.length + 1, name: user.name };
+    users.push(newUser);
+    return new Response(JSON.stringify(newUser), {
+        headers: { "Content-Type": "application/json" },
+        status: 201,
+    });
 }
